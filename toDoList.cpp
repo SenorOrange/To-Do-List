b2595868d310b69d010575ec19e66ae305091da8
Senor Orange <136025535+SenorOrange@users.noreply.github.com>
2025-01-13 10:09:19 -0800

    2025-01-13-v1

---
+++ toDoList.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
+#include <cstdlib>
+using namespace std;
+
+
+//CREATES NEW LIST OF TASKS
+void createNewList() {
+    
+}
+
+//LOADS OLD LIST OF TASKS FROM FILE
+void loadList() {
+
+}
+
+//SAVES CURRENT LIST TO FILE
+void saveList() {
+
+}
+
+//TAKES USER INPUT TO ADD A NEW TASK
+void newTasks() {
+
+}
+
+//DELETES A TASK THE USER DOSENT WANT ANYMORE
+void deleteTask() {
+
+}
+
+//CHECKS OR UNCHECKS A TASK WHETHER COMPLETED OR NOT
+void taskState() {
+
+}
+
+//TRIGGERS THE CODE
+int main() {
+
+    createNewList();
+    loadList();
+    saveList();
+    newTasks();
+    deleteTask();
+    taskState();
+
+}
