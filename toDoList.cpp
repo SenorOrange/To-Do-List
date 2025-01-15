f08efcb9a8ffcfeff89fffbe682ae035b6d74772
Senor Orange <136025535+SenorOrange@users.noreply.github.com>
2025-01-15 11:45:40 -0800

    2025-01-15-v1

--- toDoList.cpp
+++ toDoList.cpp
@@ -11,7 +11,13 @@ using namespace std;
 
 string Operation;
 string Task;
+string fileData;
+string file;
+string line;
 vector<string> toDoList;
+
+bool x = true; //USED TO KEEP THE PROGRAM RUNNING
+bool y = false; //USED TO CHECK IF A FILE IS ACTIVE
 
 //USER INPUT
 void getUserInput () {
@@ -31,7 +37,7 @@ void createNewList() {
     //CREATES A NEW FILE
     ofstream file(Task + ".txt");
 
-    cout << "New List Created Called: " << Task;
+    cout << "New List Created Called: " << Task << endl;
 }
 
 //LOADS OLD LIST OF TASKS FROM FILE
@@ -40,7 +46,18 @@ void loadList() {
     //LET USER KNOW THE TASK IS PROCESSING
     cout << "Processing Task" << endl;
     std::this_thread::sleep_for(3s);
-    cout << "Successfully Loaded: " << Task;
+
+    ifstream file(Task + ".txt");
+
+    if (!file.is_open()) {
+        cout << "No File Found" << endl;
+        return;
+    }
+
+    while (std::getline(file, fileData)) 
+        cout << fileData << endl;
+    
+    cout << "Successfully Loaded: " << Task << endl;
 }
 
 //SAVES CURRENT LIST TO FILE
@@ -51,7 +68,7 @@ void saveList() {
     std::this_thread::sleep_for(3s);
 
     ofstream file(Task + ".txt");
-    cout << "Successfully Saved: " << Task;
+    cout << "Successfully Saved: " << Task << endl;
 }
 
 //TAKES USER INPUT TO ADD A NEW TASK
@@ -60,7 +77,7 @@ void newTasks() {
     //LET USER KNOW THE TASK IS PROCESSING
     cout << "Processing Task" << endl;
     std::this_thread::sleep_for(3s);
-    cout << "Successfully Added To The List: " << Task;
+    cout << "Successfully Added To The List: " << Task << endl;
 }
 
 //DELETES A TASK THE USER DOSENT WANT ANYMORE
@@ -69,7 +86,7 @@ void deleteTask() {
     //LET USER KNOW THE TASK IS PROCESSING
     cout << "Processing Task" << endl;
     std::this_thread::sleep_for(3s);
-    cout << "Successfully Deleted From The List: " << Task;
+    cout << "Successfully Deleted From The List: " << Task << endl;
 }
 
 //CHECKS OR UNCHECKS A TASK WHETHER COMPLETED OR NOT
@@ -78,30 +95,45 @@ void taskState() {
     //LET USER KNOW THE TASK IS PROCESSING
     cout << "Processing Task" << endl;
     std::this_thread::sleep_for(3s);
-    cout << "Successfully Altered Task To: " << Task;
+
+    
+
+
+
+    size_t pos = fileData.find(Task);
+
+    cout << "Successfully Altered Task To: " << pos << endl;
 }
 
 //TRIGGERS THE CODE
 int main() {
-    getUserInput();
+    while (x = true) {
+        getUserInput();
 
-    if (Operation == "New List") {
-        createNewList();
-    } else if (Operation == "Load List") {
-        loadList();
-    } else if (Operation == "Save List") {
-        saveList();
-    } else if (Operation == "New Task") {
-        newTasks();
-    } else if (Operation == "Delete Task") {
-        deleteTask();
-    } else if (Operation == "Task State") {
-        taskState();
-    } else {
-        cout << "Invalid Operation Please Try Again";
-    }
-    
-    
+        if (Operation == "New List" || Operation == "new list") {
+            createNewList();
+        } else if (Operation == "Load List" || Operation == "load list") {
+            loadList();
+        } else if (Operation == "Save List" || Operation == "save list") {
+            saveList();
+        } else if (Operation == "New Task" || Operation == "new task") {
+            newTasks();
+        } else if (Operation == "Delete Task" || Operation == "delete task") {
+            deleteTask();
+        } else if (Operation == "Task State" || Operation == "task state") {
+            taskState();
+        } else if (Operation == "Help" || Operation == "help") {
+            cout << "New List: Creates a new list of tasks" << endl;
+            cout << "Load List: Loads an old list of tasks from a file" << endl;
+            cout << "Save List: Saves the current list to a file" << endl;
+            cout << "New Task: Takes user input to add a new task" << endl;
+            cout << "Delete Task: Deletes a task the user doesn't want anymore" << endl;
+            cout << "Task State: Checks or unchecks a task whether completed or not" << endl;
+        } else {
+            cout << "Invalid Operation Please Try Again" << endl;
+        }
+
     std::this_thread::sleep_for(20ms);
-    return 0;
+
+    }
 }
